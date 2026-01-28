af076eae0f82dbe7a063c3fd5346221e02984830
ianmuchyri <ianmuchiri8@gmail.com>
2026-01-28 15:07:46 +0300

    remove blogs
    Signed-off-by: ianmuchyri <ianmuchiri8@gmail.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,125 +1,116 @@
-import { themes as prismThemes } from 'prism-react-renderer';
-import type { Config } from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: 'SuperMQ',
-  favicon: 'img/favicon.png',
+  title: "SuperMQ",
+  favicon: "img/favicon.png",
 
-  url: 'https://docs.supermq.abstractmachines.fr',
-  baseUrl: '/',
+  url: "https://docs.supermq.abstractmachines.fr",
+  baseUrl: "/",
 
-  organizationName: 'absmach',
-  projectName: 'supermq',
+  organizationName: "absmach",
+  projectName: "supermq",
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          routeBasePath: '/',
-          sidebarPath: 'sidebars.ts',
-          editUrl:
-            'https://github.com/absmach/supermq-docs/blob/main',
-        },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          editUrl:
-            'https://github.com/absmach/supermq-docs/blob/main',
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-          blogSidebarTitle: 'All Blog Posts',
-          blogSidebarCount: 'ALL',
+          routeBasePath: "/",
+          sidebarPath: "sidebars.ts",
+          editUrl: "https://github.com/absmach/supermq-docs/blob/main",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./static/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
   ],
 
-  plugins: [
-    'docusaurus-plugin-drawio',
-  ],
+  plugins: ["docusaurus-plugin-drawio"],
   markdown: {
     mermaid: true,
   },
-  themes: ['@docusaurus/theme-mermaid'],
+  themes: ["@docusaurus/theme-mermaid"],
 
   themeConfig: {
     navbar: {
       logo: {
-        alt: 'SuperMQ Logo',
-        srcDark: 'img/logo-dark.png',
-        src: 'img/logo-light1.png',
+        alt: "SuperMQ Logo",
+        srcDark: "img/logo-dark.png",
+        src: "img/logo-light1.png",
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'smqSidebar',
-          position: 'left',
-          label: 'Docs',
+          type: "docSidebar",
+          sidebarId: "smqSidebar",
+          position: "left",
+          label: "Docs",
         },
-        { to: '/blog', label: 'Blog', position: 'left' },
         {
-          href: 'https://github.com/absmach/supermq',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/absmach/supermq",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Overview',
-              to: '/',
+              label: "Overview",
+              to: "/",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'GitHub',
-              href: 'https://github.com/absmach/supermq',
+              label: "GitHub",
+              href: "https://github.com/absmach/supermq",
             },
             {
-              label: 'Gitter',
-              href: 'https://app.gitter.im/#/room/#absmach_supermq:gitter.im?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge',
+              label: "LinkedIn",
+              href: "https://www.linkedin.com/company/abstract-machines",
             },
             {
-              label: 'Google Group',
-              href: 'https://groups.google.com/forum/#!forum/mainflux',
+              label: "Twitter",
+              href: "https://twitter.com/absmach",
             },
             {
-              label: 'Twitter',
-              href: 'hhttps://twitter.com/absmach',
+              label: "Matrix",
+              href: "https://matrix.to/#/#supermq:matrix.org",
+            },
+
+            {
+              label: "YouTube",
+              href: "https://www.youtube.com/@absmach",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'Blog',
-              to: '/blog',
+              label: "Blog",
+              href: "https://absmach.eu/blog",
+            },
+            {
+              label: "Website",
+              href: "https://absmach.eu/supermq",
             },
           ],
         },
@@ -131,7 +122,7 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
     },
     typography: {
-      fontFamily: 'Inter, sans-serif',
+      fontFamily: "Inter, sans-serif",
     },
   } satisfies Preset.ThemeConfig,
 };
